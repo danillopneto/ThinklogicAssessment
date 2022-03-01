2cc0ad7c51e08695fc5c9ff6f6188b2b901aac57
Danillo Pinheiro Neto <danillopneto@outlook.com>
2022-03-01 10:07:46 -0300

    Add project files.

---
+++ ThinklogicAssessment.UI.Web/Controllers/CalendarController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ThinklogicAssessment.Models;
+
+namespace ThinklogicAssessment.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private readonly ILogger<CalendarController> _logger;
+
+        public CalendarController(ILogger<CalendarController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Calendar Challenge
+
+1.   Create an .NET Core web site using C#.
+2.   Create a page that displays a calendar. Default the selected day to the current day.
+3.   Create a sql database to store and retrieve events.
+4.   Upon selecting a day, refresh a list of calendar events for that date on the same page.
+5.   An event should have at minimum the following fields: Start Date, End Date, Title, Location, Description
+6.   Allow a user to add events by clicking a button.
+7.   Allow a user to edit an event.
